ac6fa95623e4c20c84fd8a54d3be37670903550f
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-04-08 09:41:02 -0400

    testing

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,16 @@ use wasm_bindgen::prelude::*;
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
+
+// can't 
+#[wasm_bindgen]
+pub fn arrays(arr: js_sys::Array) -> js_sys::Array {
+    let mut out = js_sys::Array::new();
+    out.push(&JsValue::from(32));
+    out.extend(arr.iter().collect());
+    out
+}
+// pub fn arrays(warehouse_width: f32, warehouse_depth: f32, rack_width: f32, rack_depth: f32, x_coordinates: Vec<f32>, y_coordinates: Vec<f32>) -> js_sys::Array {
+//     let a = js_sys::Array::new();
+//     a
+// }
