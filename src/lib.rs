b6fdf67085630068e1c13d582a1bdb6cfb667e47
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-04-24 16:46:53 -0400

    add get walkable path function

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 use array2d::Array2D;
 use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
+use std::collections::{BinaryHeap, HashMap};
+use std::cmp::Reverse;
+
+type Location = (usize, usize);
 
 static mut WAREHOUSE_WIDTH: f32 = 0f32;
 static mut WAREHOUSE_DEPTH: f32 = 0f32;
 static mut RACK_WIDTH: f32 = 0f32;
 static mut RACK_DEPTH: f32 = 0f32;
-
+static mut MAX_ROW: i32 = 0i32;
+static mut MAX_COL: i32 = 0i32;
 // a 2d map of the warehouse
 static mut WAREHOUSE: Option<Array2D<bool>> = None;
-fn get_warehouse() -> &'static mut Array2D<bool> {
+fn get_warehouse() -> &'static Array2D<bool> {
     unsafe {
         if WAREHOUSE.is_none() {
             panic!("Run set_internal_coordinates() first");
@@ -46,7 +51,8 @@ pub fn set_internal_coordinates(
     // normalize all the values so that a single rack take up 1 cell in the internal representation of a 2d map
     let max_row = warehouse_width / rack_width;
     let max_col = warehouse_depth / rack_depth;
-    let mut warehouse = Array2D::filled_with(false, max_row.round() as usize, max_col.round() as usize);
+    let mut warehouse =
+        Array2D::filled_with(false, max_row.round() as usize, max_col.round() as usize);
     for coordinate in coordinates.iter_mut() {
         // shifts all the coordinate so that top left is 0, 0
         let x: usize = ((coordinate[0] / rack_width) + (max_row / 2f32)).floor() as usize;
@@ -55,8 +61,81 @@ pub fn set_internal_coordinates(
     }
 
     unsafe {
+        MAX_ROW = (warehouse_width / rack_width).round() as i32;
+        MAX_COL = (warehouse_depth / rack_depth).round() as i32;
         WAREHOUSE = Some(warehouse);
     }
+}
+
+fn get_distance(current_location: &Location, end_location: &Location) -> usize {
+    let diffx = (current_location.0 as f32 - end_location.0 as f32).powi(2);
+    let diffy = (current_location.1 as f32 - end_location.1 as f32).powi(2);
+    return  (diffx + diffy).sqrt().round() as usize;
+}
+
+fn get_walkable_surroudings(current: &Location) -> Vec<Location> {
+    const EIGHT_DIRECTION: [(i32, i32); 8] = [
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (1, 1),
+        (1, -1),
+        (-1, 0),
+        (-1, 1),
+        (-1, -1),
+    ];
+    let warehouse = get_warehouse();
+
+    let mut temp: Vec<Location> = vec![];
+    
+    for dir in &EIGHT_DIRECTION {
+        let x = current.0 as i32 + dir.0;
+        let y = current.1 as i32 + dir.1;
+        unsafe {
+            // check if the coordinate is in bounds
+            if x > 0 && y > 0 && x < MAX_ROW && y < MAX_COL {
+                let new_dir = (x as usize, y as usize);
+
+                // check if the location is free to walk on 
+                if !warehouse[new_dir] {
+                    temp.push(new_dir);
+                }
+            } 
+        }
+    }
+
+    return temp;
+}
+
+/**
+ * A* algorithm for calculating the pathings of two points
+ */
+#[wasm_bindgen]
+pub unsafe fn calculate_path(start_in: Vec<f32>, end_in: Vec<f32>) -> JsValue {
+    // calculate the coordinate of the start and end location
+    let start_location: Location = (((start_in[0] / RACK_WIDTH) + (MAX_ROW as f32 / 2f32)).floor() as usize,
+                 ((start_in[1] / RACK_WIDTH) + MAX_COL as f32).floor() as usize);
+    let end_location: Location = (((end_in[0] / RACK_WIDTH) + (MAX_ROW as f32 / 2f32)).floor() as usize,
+                 ((end_in[1] / RACK_WIDTH) + MAX_COL as f32).floor() as usize);
+
+    let mut queue: BinaryHeap<Reverse<Location>> = BinaryHeap::new();
+    queue.push(Reverse(start_location));
+
+
+    while !queue.is_empty() {
+        let Reverse(current) = queue.pop().unwrap(); 
+        if current == end_location {
+            break
+        }
+
+        for next_coordinate in get_walkable_surroudings(&current) {
+
+        }
+
+    }
+
+    //temp return so rust_analyzer dont error out
+    return serde_wasm_bindgen::to_value(&queue).unwrap()
 }
 
 #[wasm_bindgen]
@@ -66,7 +145,7 @@ pub fn testing() -> JsValue {
     for i in warehouse.columns_iter() {
         let mut temp: Vec<i32> = vec![];
         for ele in i {
-            temp.push(if ele.to_owned() {1} else {0});
+            temp.push(if ele.to_owned() { 1 } else { 0 });
         }
         out.push(temp);
     }
@@ -74,9 +153,3 @@ pub fn testing() -> JsValue {
     serde_wasm_bindgen::to_value(&out).unwrap()
 }
 
-#[wasm_bindgen]
-pub fn calculate_path(from: Vec<f32>, to: Vec<f32>) -> JsValue {
-    let mut paths_found: Vec<f32> = Vec::new();
-
-    serde_wasm_bindgen::to_value(&paths_found).unwrap()
-}
