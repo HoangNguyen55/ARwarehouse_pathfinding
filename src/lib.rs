a0ef600e12308bd78b1e547c570ff13c997d7ba7
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-04-24 19:47:52 -0400

    Location struct now have costs with coordinates

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,53 @@
 use array2d::Array2D;
 use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
-use std::collections::{BinaryHeap, HashMap};
-use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashSet};
+use std::cmp::{Reverse, Ordering};
 use std::marker::Copy;
 use std::convert::{From, Into};
 
-type Location = (usize, usize);
+#[derive(Eq)]
+struct Location {
+    x: usize,
+    y: usize,
+    g_cost: usize,
+    h_cost: usize 
+}
+
+impl Ord for Location {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.get_f_cost().cmp(&other.get_f_cost())
+    }
+}
+
+impl PartialOrd for Location {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Location {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_f_cost() == other.get_f_cost()
+    }
+}
+
+impl Location {
+    pub fn new(x: usize, y: usize) -> Location {
+        return Location {
+            x,
+            y,
+            g_cost: 0usize,
+            h_cost: 0usize 
+        }
+    }
+    pub fn get_f_cost(&self) -> usize {
+        return self.h_cost + self.g_cost;
+    }
+    pub fn get_coordinate(&self) -> (usize, usize) {
+        return (self.x.clone(), self.y.clone());
+    }
+}
 
 static mut WAREHOUSE_WIDTH: f32 = 0f32;
 static mut WAREHOUSE_DEPTH: f32 = 0f32;
@@ -54,7 +95,7 @@ pub unsafe fn set_internal_coordinates(
     let mut warehouse = Array2D::filled_with(false, MAX_ROW as usize, MAX_COL as usize);
     for coordinate in coordinates.iter_mut() {
         // shifts all the coordinate so that 0, 0 is middle bottom
-        let coord: Location = get_grid_coordinate(coordinate.to_owned());
+        let coord: (usize, usize) = get_grid_coordinate(coordinate.to_owned()).get_coordinate();
         warehouse[coord] = true;
     }
 
@@ -62,8 +103,8 @@ pub unsafe fn set_internal_coordinates(
 }
 
 fn get_distance(current_location: &Location, end_location: &Location) -> usize {
-    let diffx = (current_location.0 as f32 - end_location.0 as f32).powi(2);
-    let diffy = (current_location.1 as f32 - end_location.1 as f32).powi(2);
+    let diffx = (current_location.x as f32 - end_location.x as f32).powi(2);
+    let diffy = (current_location.y as f32 - end_location.y as f32).powi(2);
     return  (diffx + diffy).sqrt().round() as usize;
 }
 
@@ -83,15 +124,15 @@ fn get_walkable_surroudings(current: &Location) -> Vec<Location> {
     let mut temp: Vec<Location> = vec![];
     
     for dir in &EIGHT_DIRECTION {
-        let x = current.0 as i32 + dir.0;
-        let y = current.1 as i32 + dir.1;
+        let x = current.x as i32 + dir.0;
+        let y = current.y as i32 + dir.1;
         unsafe {
             // check if the coordinate is in bounds
             if x > 0 && y > 0 && x < MAX_ROW && y < MAX_COL {
-                let new_dir = (x as usize, y as usize);
+                let new_dir = Location::new(x as usize, y as usize);
 
                 // check if the location is free to walk on 
-                if !warehouse[new_dir] {
+                if !warehouse[new_dir.get_coordinate()] {
                     temp.push(new_dir);
                 }
             } 
@@ -106,29 +147,32 @@ where T:  From<f32>
         + Into<f32>
         + Copy
 {
-    let x_converted: usize = ((input_coordinates[0].into() / RACK_WIDTH) + (MAX_ROW as f32 / 2f32)).floor() as usize;
-    let y_converted: usize = ((input_coordinates[1].into() / RACK_WIDTH) + MAX_COL as f32).floor() as usize;
+    let x: usize = ((input_coordinates[0].into() / RACK_WIDTH) + (MAX_ROW as f32 / 2f32)).floor() as usize;
+    let y: usize = ((input_coordinates[1].into() / RACK_WIDTH) + MAX_COL as f32).floor() as usize;
 
-    return (x_converted, y_converted)
+    return Location::new(x, y);
 }
 
 /**
  * A* algorithm for calculating the pathings of two points
  */
-//#[wasm_bindgen]
+#[wasm_bindgen]
 pub unsafe fn calculate_path(start_in: Vec<f32>, end_in: Vec<f32>) -> JsValue {
     // calculate the coordinate of the start and end location
     let start_location: Location = get_grid_coordinate(start_in);
     let end_location: Location = get_grid_coordinate(end_in);
 
+    // create a priority queue but the queue is currently a max-heap
     let mut queue: BinaryHeap<Reverse<Location>> = BinaryHeap::new();
+    // need to use Reverse to have a min-heap
     queue.push(Reverse(start_location));
 
+    let mut visited: HashSet<Location> = HashSet::new(); 
 
     while !queue.is_empty() {
         let Reverse(current) = queue.pop().unwrap(); 
         if current == end_location {
-            break
+            break;
         }
 
         for next_coordinate in get_walkable_surroudings(&current) {
@@ -138,7 +182,7 @@ pub unsafe fn calculate_path(start_in: Vec<f32>, end_in: Vec<f32>) -> JsValue {
     }
 
     //temp return so rust_analyzer dont error out
-    return serde_wasm_bindgen::to_value(&queue).unwrap()
+    return serde_wasm_bindgen::to_value(&visited).unwrap();
 }
 
 #[wasm_bindgen]
