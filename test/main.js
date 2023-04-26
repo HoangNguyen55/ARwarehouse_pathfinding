c289a30c48d0ecc6e414c6fccf4a0ff2ec875723
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-04-25 21:57:00 -0400

    display the warehouse and path with graphic

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-import init, { set_internal_coordinates, calculate_path } from './pkg/arwarehouse_pathfinding.js'
+import init, { set_internal_coordinates, testing, calculate_path } from './pkg/arwarehouse_pathfinding.js'
 
 async function parseARD(path) {
     //convert feet to metre, value of ard are feet.
@@ -7,7 +7,8 @@ async function parseARD(path) {
         rackDepth: null,
         warehouseWidth: null,
         warehouseDepth: null,
-        locations: []
+        locations: [],
+        pickings: []
     }
 
     const text = await (await fetch(path, { headers: { "Content-Type": "text/xml" } })).text();
@@ -31,19 +32,56 @@ async function parseARD(path) {
         });
     });
 
+    const picks = Array.from(xmlDoc.getElementsByTagName("picklocation"));
+    picks.forEach((pick) => {
+        let x = pick.getAttribute("x") / 3.281;
+        let z = pick.getAttribute("y") / 3.281;
+        array['pickings'].push([x,z]);
+    });
+
+
     return array
 }
 
-async function run() {
-    await init(fetch('./pkg/arwarehouse_pathfinding_bg.wasm'))
-    const arr = await parseARD('./fishbone.ard');
+function find_path(pick) {
+    grid = testing();
+    let path_found = calculate_path([0, 0], [pick[0], pick[1]]);
+    path_found.forEach((i) => {
+        let x = Math.floor((i[1] / arr['rackWidth']) + (max_row / 2));
+        let y = Math.floor((i[0] / arr['rackDepth']) + max_col);
+        grid[x][y] = 2;
+    })
 
-    set_internal_coordinates(arr['warehouseWidth'], arr['warehouseDepth'], arr['rackWidth'], arr['rackDepth'], arr['locations']);
-    // let grid = testing();
-    let path_found = calculate_path([0, 0], [74 / 3.281, -16 / 3.281]);
-
-    console.log(path_found);
-
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 2) {
+                ctx.fillStyle = "green";
+                ctx.fillRect(j * 10, i * 10, 10, 10);
+            } else if (grid[i][j] === 1) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(j * 10, i * 10, 10, 10);
+            }
+        }
+    }
 }
 
-run()
+await init(fetch('./pkg/arwarehouse_pathfinding_bg.wasm'))
+const arr = await parseARD('./fishbone.ard');
+
+set_internal_coordinates(arr['warehouseWidth'], arr['warehouseDepth'], arr['rackWidth'], arr['rackDepth'], arr['locations']);
+let grid = testing();
+const max_row = Math.round(arr['warehouseWidth'] / arr['rackWidth']);
+const max_col = Math.round(arr['warehouseDepth'] / arr['rackDepth']);
+const canvas = document.getElementById("canvas");
+canvas.width = grid[0].length * 10;
+canvas.height = grid.length * 10;
+const ctx = canvas.getContext("2d");
+let t = -3001;
+
+arr['pickings'].forEach((pick) => {
+    t += 3000;
+    setTimeout(() => { 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        find_path(pick)
+    }, t);
+})
